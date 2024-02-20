c3cc66d95ff610fc8d7632a76f429bb8f6fff002
biggyeatsock <loganluper6678@gmail.com>
2024-02-20 17:04:31 -0500

    text file work, now database time

--- main.py
+++ main.py
@@ -10,39 +10,43 @@
 #####################
 
 
-# "r" - Read - Default value. Opens a file for reading, error if the file does not exist
-
-# "a" - Append - Opens a file for appending, creates the file if it does not exist
-
-# "w" - Write - Opens a file for writing, creates the file if it does not exist
-
-# "x" - Create - Creates the specified file, returns an error if the file exists
-
-
-
-def redata():
-    data =  open('data.txt', 'r') # reads the data from the file
+def redata(): # Read the data from the file
+    data =  open('data.txt', 'r') 
     for line in data:
         print(f'\n{line}')
+    return data
 
 
 def write_info(data): # writes the info to the file.
+    data = open('data.txt', 'a') # opens the file for appending.
     wcheck = input("Would you like to write to the file? (y/n) \n > ")
     if wcheck == "y":
         uinput = input("What would you like to write to the file? \n > ")
-        data.write(uinput)
-    elif wcheck == "n":
+        try:
+            if len(uinput) != 0: # Checks if the user wrote anything.
+                print(f"\nWriting to the file...\n")
+                data.write('\n'+uinput) # Writes the user input to the file.
+            else: # Prints the error
+                raise ValueError("You didn't write anything, so no changes made.")
+        except ValueError as e:
+            print(e)
+    else:
         print("\nOkay, no changes made.\n")
-    return data
 
+def userclose():
+    uinput = input("Would you like to close the program? (y/n) \n > ")
+    if uinput == "y":
+        print("Closing the program...")
+        exit()
 
 
 def main(): # Main function that runs the program.
     print("Welcome to the BookWise Book Manager")
-    redata() # reads the data from the file
-    
-    # write_info(data)
-    # print(data)
+    while True: # Loops the Program.
+        data = redata() # reads the data from the file
+        write_info(data)
+        userclose()
+
 
 
 
